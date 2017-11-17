0280bdd5a5e09a551b004aec985eb27af6028cb0
Michel Souza <Michel Souza>
2017-11-17 11:13:30 -0200

    colocar o servico envio de email para funcionar

--- webrelatorio/app/email/sql/product.js
+++ webrelatorio/app/email/sql/product.js
@@ -1,3 +1,10 @@
-var sql = `select * from sys.product`;
+var sql = `select 
+            CASE WHEN MOD(ROWNUM,2) = 0 THEN '#D3D3D3' 
+                        ELSE 'white'
+                        END COLOR,
+            c.PRODUCT_ID,
+            c.NAME,
+            c.VALUE
+            from sys.product c`;
 
 module.exports = sql;                
