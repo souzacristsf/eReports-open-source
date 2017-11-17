fea399f604684231dbe99b9328723be6c0e1a59f
Michel Souza <Michel Souza>
2017-11-17 12:01:54 -0200

    arrumando mardown

---
+++ webrelatorio/app/email/config/user.js
@@ -0,0 +1,10 @@
+module.exports = {
+  user          : 'testopensource@hotmail.com', //inserir o email seu usuario aqui
+
+  pass      : '<123456>', ////inserir a senha do email seu usuario aqui
+
+  remetente: 'Send Reports Email <souza_crists@hotmail.com>',
+
+  assunto: 'Indicadores de Produtos'
+
+};
