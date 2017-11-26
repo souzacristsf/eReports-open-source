eb8ad444a9278e18bda0df3e24f6d73e83d57245
Michel Souza <souza_crists@hotmail.com>
2017-11-26 13:00:54 -0200

    organizando

--- eReports/app/routes/report.js
+++
@@ -1,7 +0,0 @@
-module.exports = app => {
-    const url = `${app.url}/report`
-    const Controllers = app.controllers.report
-
-    app.route(`${url}/:sequencia`)
-        .get(Controllers.listAll)
-}
--- eReports/app/routes/ruleReport.js
+++
@@ -1,8 +0,0 @@
-module.exports = app => {
-    const url = `${app.url}/rulereport`
-    const Controllers = app.controllers.ruleReport
-    const isAdmin = app.middleware.permission
-
-    app.route(`${url}/new`)
-        .get(isAdmin, Controllers.create)
-}
